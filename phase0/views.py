8bb10066c62fbd209598bd052d359ae63e2bcc66
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2016-11-18 16:28:58 +0330

    little chang

--- phase0/views.py
+++ phase0/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseBadRequest
 from rest_framework.views import APIView
 
 from helpers.permissions import TeamPermission
@@ -15,11 +15,11 @@ class Phase0View(APIView):
         member = request.data.get('member')
         text = request.team.text
 
-        checklist = {'1': text.smallest_repeated_word, '2': text.distinct_longest_words}
+        checklist = {'1': text.smallest_repeated_word, '2': str(text.distinct_longest_words)}
         if member in checklist:
             if answer == checklist[member]:
                 return HttpResponse("Correct answer! You've completed your phase0 part! Congrats!")
             else:
                 return HttpResponse("Wrong answer:( Keep trying...")
-        return HttpResponse("Invalid request data! Check you declared 'member' correctly"
-                            " in your request. It should be either '1' or '2' ")
+        return HttpResponseBadRequest("Invalid request data! Check you declared 'member' correctly"
+                                      " in your request. It should be either '1' or '2' ")
