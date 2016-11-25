0aa7efd3320699a2ef572f53cba78ac4494be3c2
somagh <aryakowsary@yahoo.com>
2016-11-25 22:33:54 +0330

    static

--- fop/settings.py
+++ fop/settings.py
@@ -121,9 +121,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = 'welcome/'
+LOGIN_REDIRECT_URL = '/welcome/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
