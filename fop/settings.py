6014394a29c8a264022a2c17937188f9808bf5b9
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2016-11-25 22:13:27 +0330

    little change

--- fop/settings.py
+++ fop/settings.py
@@ -121,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = 'welcome/'
+LOGIN_REDIRECT_URL = '/welcome/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
