f94ec09d229452523ce02e2f3364d9954f3a5f42
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2016-11-25 22:21:55 +0330

    little change

--- teams/urls.py
+++ teams/urls.py
@@ -7,5 +7,5 @@ from teams.views import WelcomeView
 urlpatterns= [
     url(r'^welcome/$', WelcomeView.as_view(), name='welcome'),
     url(r'^login/$', auth_views.login,{'template_name': 'teams/login.html'}, name="login"),
-    url(r'^logout/$', auth_views.logout,{'next_page': 'login/'}, name='logout'),
+    url(r'^logout/$', auth_views.logout,{'next_page': '/login/'}, name='logout'),
 ]
