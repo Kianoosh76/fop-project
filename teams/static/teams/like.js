cda308dc47034adf8d6869a8b0a227551db665d7
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2016-11-26 22:14:02 +0330

    changes in http file

--- teams/static/teams/like.js
+++ teams/static/teams/like.js
@@ -1,5 +1,19 @@
-$(document).load(function(){
+$(document).ready(function(){
     $(".like").click(function(){
-        document.write("salam");
+        var row = $(this).attr('row');
+        $.ajax({
+            url: '/like',
+            method: 'POST',
+            data: {
+                'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
+                'team': $("#"),
+                'member': $("#members:selected").val()
+            },
+            success: function(data, status, xhttp){
+            },
+            error: function(data, status, xhttp){
+                alert(data.responseText);
+            }
+        });
     });
 });
