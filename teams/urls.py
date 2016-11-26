97d9660448f8d0dcaf43f4b959dda079fc68cdc5
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2016-11-26 20:25:22 +0330

    like view implemented

--- teams/urls.py
+++ teams/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.urls import reverse
 
-from teams.views import WelcomeView, TeamsList
+from teams.views import WelcomeView, TeamsList, LikeView
 
 urlpatterns= [
     url(r'^welcome/$', WelcomeView.as_view(), name='welcome'),
     url(r'^login/$', auth_views.login,{'template_name': 'teams/login.html'}, name="login"),
     url(r'^logout/$', auth_views.logout,{'next_page': '/login/'}, name='logout'),
-    url(r'^$', TeamsList.as_view(), name='list')
+    url(r'^like/$', LikeView.as_view(), name='like'),
+    url(r'^$', TeamsList.as_view(), name='list'),
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 
-from helpers.permissions import TeamPermission, PermissionCheckerMixin
-from teams.models import Team
+from helpers.permissions import TeamPermission, PermissionCheckerMixin, AjaxPermission
+from teams.models import Team, Vote
 
 
 class WelcomeView(APIView):
@@ -19,3 +20,18 @@ class TeamsList(ListView):
     model = Team
     template_name = 'teams/list.html'
     context_object_name = 'teams'
+
+
+class LikeView(APIView):
+    permission_classes = [AjaxPermission, TeamPermission]
+
+    def post(self, request, *args, **kwargs):
+        member = get_object_or_404(request.team.members,
+                                   student_id=int(request.POST.get('member', -1)))
+        team = get_object_or_404(Team, user__username=request.POST.get('team'))
+        vote, created = Vote.objects.get_or_create(member=member, team=team)
+        if not created:
+            vote.valid ^= 1
+            vote.save()
+        team.refresh_from_db()
+        return HttpResponse(team.votes)
