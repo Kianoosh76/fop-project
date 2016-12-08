fce257be387e8d8ae090a8f0681f1e21e7caf95a
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2016-12-08 18:54:57 +0330

    bug fix on like

--- teams/models.py
+++ teams/models.py
@@ -1,6 +1,6 @@
 from Crypto.Random.random import randint, shuffle
 from django.contrib.auth.models import User
-from django.db import models
+from django.db import models, transaction
 from django.db.models.deletion import SET_NULL
 
 from phase0.models import Text
@@ -74,17 +74,18 @@ class Vote(models.Model):
         unique_together = ['member', 'team']
 
     def save(self, *args, **kwargs):
-        if self.pk is None:
-            self.team.votes += 1
-            self.team.save()
-        else:
-            old_object = Vote.objects.get(pk=self.pk)
-            if old_object.valid and not self.valid:
-                self.team.votes -= 1
-            elif not old_object.valid and self.valid:
+        with transaction.atomic():
+            if self.pk is None:
                 self.team.votes += 1
-            self.team.save()
-        super().save(*args, **kwargs)
+                self.team.save()
+            else:
+                old_object = Vote.objects.get(pk=self.pk)
+                if old_object.valid and not self.valid:
+                    self.team.votes -= 1
+                elif not old_object.valid and self.valid:
+                    self.team.votes += 1
+                self.team.save()
+            super().save(*args, **kwargs)
 
     def __str__(self):
         if self.valid:
